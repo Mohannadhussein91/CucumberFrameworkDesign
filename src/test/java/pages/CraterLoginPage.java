e72ff39a59bbf424422d6448a9ef0838f713048d
Mohannadhussein91 <119636775+Mohannadhussein91@users.noreply.github.com>
2023-01-03 20:11:22 -0700

    Adding new scenario

---
+++ src/test/java/pages/CraterLoginPage.java
@@ -0,0 +1,61 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utils.BrowserUtils;
+import utils.Driver;
+import utils.TestDataReader;
+
+public class CraterLoginPage {
+	public CraterLoginPage() {
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+	
+	@FindBy (xpath = "//div[@name ='email']/input")
+	public WebElement useremail;
+	
+	@FindBy (xpath = "//div[@name ='password']/input")
+	public WebElement password;
+	
+	@FindBy (linkText = "Forgot Password")
+	public WebElement forgotpassword;
+	
+	@FindBy (xpath = "//button[text()='Login']")
+	public WebElement loginButton;
+	
+	@FindBy (xpath = "//p[contains(text(), 'Copyright @')]")
+	public WebElement copyRightText;
+	
+	@FindBy (xpath = "//h1[contains(text(), 'Simple Invoicing for')]")
+	public WebElement businessTagLine;
+	
+	@FindBy (xpath = "//p[contains(text(), 'Crater helps you track expenses')]")
+	public WebElement businessSubText;
+	
+	@FindBy (xpath = "//p[contains(text(), 'These credentials do not match our records.')]")
+	public WebElement invalidUserErrorMessage;
+	
+	@FindBy (xpath = "//span[text()='Field is required']")
+	public WebElement fieldRequired;
+	
+	BrowserUtils utils;
+	
+	public void login() throws InterruptedException {
+		
+//		  useremail.sendKeys(TestDataReader.getProperty("craterValidUserEmail"));
+//		  Thread.sleep(1000);
+//		  password.sendKeys(TestDataReader.getProperty("craterValidPassword"));
+//		  loginButton.click();
+		// this way to make it work with chrome using Action
+		utils = new BrowserUtils();
+		utils.sendKeysWithActionsClass(useremail, TestDataReader.getProperty("craterValidUserEmail"));
+		Thread.sleep(1000);
+		utils.sendKeysWithActionsClass(password, TestDataReader.getProperty("craterValidPassword"));
+		loginButton.click();
+	}
+	
+
+
+}
--- src/test/java/step_defintions/UserManagementSteps.java
+++ src/test/java/step_defintions/UserManagementSteps.java
@@ -1,30 +1,47 @@
 package step_defintions;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.CraterDashboardPage;
+import pages.CraterLoginPage;
+import utils.BrowserUtils;
+import utils.Driver;
+import utils.TestDataReader;
 
 public class UserManagementSteps {
 	
+	CraterLoginPage craterLogin = new CraterLoginPage();
+	CraterDashboardPage dashboard = new CraterDashboardPage();
+	BrowserUtils utils = new BrowserUtils();
+	
 	@Given("user is on the login page")
 	public void user_is_on_the_login_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	  Driver.getDriver().get(TestDataReader.getProperty("craterUrl"));
+	  Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  
 	}
-	@When("user enters valid username and password")
-	public void user_enters_valid_username_and_password() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	@When("user enters valid {string} and {string}")
+	public void user_enters_valid_and(String username, String password) {
+		utils.sendKeysWithActionsClass(craterLogin.useremail, username);
+		utils.sendKeysWithActionsClass(craterLogin.password, password);
+	 
 	}
 	@When("clicks on the login button")
 	public void clicks_on_the_login_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		craterLogin.loginButton.click();
+	  
 	}
+	
 	@Then("user should be on the dashboard page")
 	public void user_should_be_on_the_dashboard_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   utils.waitUntilElementVisible(dashboard.amountDueText);
+	   Assert.assertTrue(dashboard.amountDueText.isDisplayed());
+	  
 	}
 
 }
